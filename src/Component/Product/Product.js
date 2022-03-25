a9d092c6caa061b4b3a3ae37f6f8cfdd9d8de8b1
Safayet <mrishafayet003@gmail.com>
2022-03-26 01:32:32 +0600

    added button and design cart

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import './Product.css'
+import { FaShoppingCart } from "@react-icons/all-files/fa/FaShoppingCart";
 
-const Product = ({ products }) => {
+const Product = ({ products, addToCard }) => {
     const { id, name, img, price, Brand } = products;
     // console.log(products)
     return (
-        <div>
+        <div className='all-product'>
             <img src={img} alt="" />
-            <div>
-                <h2>Name: {name} </h2>
-                <h3>Price: {price}</h3>
+            <div className='product-deatails'>
+                <h3>Name: {name} </h3>
+                <h3>Price: ${price}</h3>
                 <h3>Brand: {Brand} </h3>
             </div>
+            <button onClick={addToCard} className='button-cart'>
+                <p className='button-text'> Add to Cart </p>
+                <FaShoppingCart />
+            </button>
         </div>
     );
 };
--- src/Component/Products/Products.js
+++ src/Component/Products/Products.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
+import './Products.css';
 
 const Products = () => {
     // console.log(props)
+    const addToCard = () => {
+        console.log('added')
+    }
     const [products, setProducts] = useState([]);
     useEffect(() => {
         fetch('data.json')
@@ -10,13 +14,16 @@ const Products = () => {
             .then(data => setProducts(data))
     }, []);
     return (
-        <div>
-            <div className=''>
+        <div className='shop-container'>
+            <div className='proudcts-container'>
                 {
                     products.map(
-                        (products) => <Product key={products.id} products={products}></Product>
+                        (products) => <Product key={products.id} products={products} addToCard={addToCard}></Product>
                     )
                 }
+            </div>
+            <div className='selected-products'>
+                <h2>selected proudcts</h2>
             </div>
         </div>
     );
